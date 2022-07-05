84923be8f55348ef10ec7d2d8cb7fe5a12e6ed4e
GoGei <rich.290401@gmail.com>
2022-07-05 23:05:45 +0300

    Problem 3 solved

--- problem_2.py
+++ problem_2.py
@@ -10,7 +10,6 @@ res = 0
 a, b = 1, 2
 while a <= 4_000_000:
     if a % 2 == 0:
-        print(a)
         res += a
     a, b = b, a+b
 
---
+++ problem_3.py
@@ -0,0 +1,23 @@
+"""
+The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143 ?
+"""
+
+target = 600851475143
+lim = int(target ** 0.5)
+res = 1
+
+for i in range(2, lim):
+    if target % i == 0:
+        count = 1
+        for j in range(2, (i // 2 + 1)):
+            if i % j == 0:
+                count = 0
+                break
+        if count == 1 and res <= i:
+            res = i
+
+print(res)
+ANSWER = 6857
+assert ANSWER == res
