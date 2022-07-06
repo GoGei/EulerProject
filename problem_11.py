126a890587d264a579518fd4a1196748d2af1e26
GoGei <rich.290401@gmail.com>
2022-07-06 22:39:09 +0300

    Problem 11 added description

--- problem_11.py
+++ problem_11.py
@@ -26,6 +26,8 @@ The product of these numbers is 26 × 63 × 78 × 14 = 1788696.
 
 What is the greatest product of four adjacent numbers in the same direction (up, down, left, right, or diagonally) in the 20×20 grid?
 """
+import functools
+import operator
 
 raw_matrix = """08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08
 49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00
@@ -53,3 +55,48 @@ matrix = []
 for line in raw_matrix.split('\n'):
     converted = list(map(int, line.split()))
     matrix.append(converted)
+
+res = 0
+iterating = range(1, path_len + 1)
+
+
+def current(iterable):
+    return functools.reduce(operator.mul, iterable)
+
+
+for i in range(len(matrix)):
+    for j in range(len(matrix[0])):
+        j_plus_cond = j + (path_len + 1) <= len(matrix[0])
+        j_minus_cond = j - (path_len + 1) >= 0
+        i_plus_cond = i + (path_len + 1) <= len(matrix[0])
+        i_minus_cond = i - (path_len + 1) >= 0
+
+        if j_minus_cond:
+            curr = current([matrix[i][j - k] for k in iterating])
+            res = max(curr, res)
+        if j_plus_cond:
+            curr = current([matrix[i][j + k] for k in iterating])
+            res = max(curr, res)
+        if i_minus_cond:
+            curr = current([matrix[i - k][j] for k in iterating])
+            res = max(curr, res)
+        if i_plus_cond:
+            curr = current([matrix[i + k][j] for k in iterating])
+            res = max(curr, res)
+
+        if j_plus_cond and i_plus_cond:
+            curr = current([matrix[i + k][j + k] for k in iterating])
+            res = max(curr, res)
+        if j_plus_cond and i_minus_cond:
+            curr = current([matrix[i - k][j + k] for k in iterating])
+            res = max(curr, res)
+        if j_minus_cond and i_plus_cond:
+            curr = current([matrix[i + k][j - k] for k in iterating])
+            res = max(curr, res)
+        if j_minus_cond and i_minus_cond:
+            curr = current([matrix[i - k][j - k] for k in iterating])
+            res = max(curr, res)
+
+print(res)
+ANSWER = 70600674
+assert ANSWER == res
