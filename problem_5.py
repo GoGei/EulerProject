415fd8eb4b85ce87a814287ab1b4fec6303eecb2
GoGei <rich.290401@gmail.com>
2022-07-06 12:04:27 +0300

    Problem 5 solution added

--- problem_5.py
+++ problem_5.py
@@ -3,3 +3,31 @@
 
 What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 """
+
+
+dividers = set(range(1, 21))
+found = False
+i = max(dividers)
+res = 0
+lim = 500000000
+while True:
+    for j in dividers:
+        if i % j != 0:
+            found = False
+            break
+        found = True
+
+    if i % 50_000_000 == 0:
+        print('%s is passed' % i)
+
+    if found:
+        res = i
+        break
+    else:
+        i += 10
+        if i >= lim:
+            raise ValueError('limit is reached')
+
+print(res)
+ANSWER = 232792560
+assert ANSWER == res
